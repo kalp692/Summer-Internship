05ed5235ddadb06eabda04910da343d28d729895
kalp692 <84793959+kalp692@users.noreply.github.com>
2021-05-28 18:48:20 +0530

    Add files via upload

---
+++ Day 2/main activity.java
@@ -0,0 +1,15 @@
+05.28 18:46
+package com.dreamproject.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
